28070ed1c3108ac7abe9b979818f32a0aa0c0e43
pdhakal07 <52260690+pdhakal07@users.noreply.github.com>
2020-11-02 15:05:12 -0500

    Update ec2-terraform.tf
    Create ec2 instance.

---
+++ ec2-terraform.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0947d2ba12ee1ff75"
+  instance_type = "t2.micro"
+        }
+       
